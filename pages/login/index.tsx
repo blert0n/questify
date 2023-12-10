239b4f21d970c9f6e416bbd0ce30ab25865ebd45
Blerton <isufib20@gmail.com>
2023-12-10 17:47:45 +0100

    clerk test

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,10 @@
 import Meta from "@/components/Layout/Title";
 import Login from "@/components/User/Login";
+import { useClerk } from "@clerk/nextjs";
 
 export default function Index() {
+  const { loaded } = useClerk();
+  console.log(loaded, "loaded");
   return (
     <>
       <Meta title="Questify - Login" />
