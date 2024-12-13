6e879e7bd261428aa8e0236ea199f38032f8fa84
Blerton <bi46173@ubt-uni.net>
2024-12-13 02:23:01 +0100

    migration fail

--- questify-hasura/migrations/questify/1734051643383_changing type column in formItem/up.sql
+++ questify-hasura/migrations/questify/1734051643383_changing type column in formItem/up.sql
@@ -4,7 +4,7 @@ ADD COLUMN type_new VARCHAR(255);
 UPDATE "public"."FormItem"
 SET "type_new" = ft.type
 FROM "public"."FormItemType" ft
-WHERE "public"."FormItem"."type" = ft.type;
+WHERE "public"."FormItem"."type"::text = ft.type;
 
 ALTER TABLE "public"."FormItem"
 DROP COLUMN type;
